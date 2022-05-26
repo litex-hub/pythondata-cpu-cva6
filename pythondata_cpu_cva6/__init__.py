afc3b10a0627ef73fdaca843ad88aa54200beb97
LiteX Robot <robot@timvideos.us>
2022-05-26 16:39:31 +0000

    Updating pythondata-cpu-cva6 to 4.2.0.post389
    Updated data to v4.2.0-253-g75807530 based on 75807530f26ba9a0ca501e9d3a6575ec375ed7ab from https://github.com/openhwgroup/cva6.
    > commit 75807530f26ba9a0ca501e9d3a6575ec375ed7ab
    > Author: Steffen Persvold <spersvold@users.noreply.github.com>
    > Date:   Thu May 12 10:46:40 2022 +0200
    >
    >     Add support for "high" counter CSRs in 32-bit mode (#847)
    >
    >     * Add support for "high" counter CSRs in 32-bit mode
    >
    >     In 32bit mode MCYCLEH, MINSTRETH, CYCLEH, TIMEH and INSTRETH are
    >     used to return the most significant 32-bit value of the counters
    >     which are now always 64-bit wide.
    >
    >     Signed-off-by: Steffen Persvold <spersvold@gmail.com>
    >
    >     * Enable writing of MCYCLEH and MINSTRETH CSRs
    >
    >     Signed-off-by: Steffen Persvold <spersvold@gmail.com>
    >
    
    Updated using 0.0.post136 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cva6/__init__.py
+++ pythondata_cpu_cva6/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cva6"
 
 # Module version
-version_str = "4.2.0.post387"
-version_tuple = (4, 2, 0, 387)
+version_str = "4.2.0.post389"
+version_tuple = (4, 2, 0, 389)
 try:
     from packaging.version import Version as V
-    pversion = V("4.2.0.post387")
+    pversion = V("4.2.0.post389")
 except ImportError:
     pass
 
@@ -44,11 +44,11 @@ Date:   Thu May 12 10:46:40 2022 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post134"
-tool_version_tuple = (0, 0, 134)
+tool_version_str = "0.0.post136"
+tool_version_tuple = (0, 0, 136)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post134")
+    ptool_version = V("0.0.post136")
 except ImportError:
     pass
 
