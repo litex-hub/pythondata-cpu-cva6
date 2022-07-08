a316ffd2d0c5aa25b1bb34e89e91f8b4dd4d6b6b
LiteX Robot <robot@timvideos.us>
2022-07-08 09:13:42 +0000

    Updating pythondata-cpu-cva6 to 4.2.0.post410
    Updated data to v4.2.0-268-g011edf49 based on 011edf49bdbc08b3ef62283406cf129d48c29372 from https://github.com/openhwgroup/cva6.
    > commit 011edf49bdbc08b3ef62283406cf129d48c29372
    > Author: Moritz Schneider <Moschn@users.noreply.github.com>
    > Date:   Fri Jul 8 10:21:57 2022 +0200
    >
    >     Fix exception type on PMP check during PTW (#908)
    >
    >     Fixes #906
    >
    >     According to the spec:
    >     > If accessing pte violates a PMA or PMP check, raise an access-fault
    >     > exception corresponding to the original access type.
    >
    >     Found by @Phantom1003 and @ProjectDimlight
    >
    >     Signed-off-by: Moritz Schneider <moritz.schneider@inf.ethz.ch>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cva6/__init__.py
+++ pythondata_cpu_cva6/__init__.py
@@ -4,30 +4,40 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cva6"
 
 # Module version
-version_str = "4.2.0.post409"
-version_tuple = (4, 2, 0, 409)
+version_str = "4.2.0.post410"
+version_tuple = (4, 2, 0, 410)
 try:
     from packaging.version import Version as V
-    pversion = V("4.2.0.post409")
+    pversion = V("4.2.0.post410")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "4.2.0.post267"
-data_version_tuple = (4, 2, 0, 267)
+data_version_str = "4.2.0.post268"
+data_version_tuple = (4, 2, 0, 268)
 try:
     from packaging.version import Version as V
-    pdata_version = V("4.2.0.post267")
+    pdata_version = V("4.2.0.post268")
 except ImportError:
     pass
-data_git_hash = "01c89b7606bc14201e985a0bbca2ca2894b6af6b"
-data_git_describe = "v4.2.0-267-g01c89b76"
+data_git_hash = "011edf49bdbc08b3ef62283406cf129d48c29372"
+data_git_describe = "v4.2.0-268-g011edf49"
 data_git_msg = """\
-commit 01c89b7606bc14201e985a0bbca2ca2894b6af6b
-Author: Yan <phantom@zju.edu.cn>
-Date:   Fri Jul 8 16:10:37 2022 +0800
+commit 011edf49bdbc08b3ef62283406cf129d48c29372
+Author: Moritz Schneider <Moschn@users.noreply.github.com>
+Date:   Fri Jul 8 10:21:57 2022 +0200
 
-    fix sfence.vma decoder (#921)
+    Fix exception type on PMP check during PTW (#908)
+    
+    Fixes #906
+    
+    According to the spec:
+    > If accessing pte violates a PMA or PMP check, raise an access-fault
+    > exception corresponding to the original access type.
+    
+    Found by @Phantom1003 and @ProjectDimlight
+    
+    Signed-off-by: Moritz Schneider <moritz.schneider@inf.ethz.ch>
 
 """
 
