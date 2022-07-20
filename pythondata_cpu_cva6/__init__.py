8ccb567eb8cd1d07f398343e74ae49bc6ca74516
LiteX Robot <robot@timvideos.us>
2022-07-20 15:14:22 +0000

    Updating pythondata-cpu-cva6 to 4.2.0.post412
    Updated data to v4.2.0-270-gb6c1d04b based on b6c1d04b6fa022c2e2df0b287865d28a0d42e756 from https://github.com/openhwgroup/cva6.
    > commit b6c1d04b6fa022c2e2df0b287865d28a0d42e756
    > Author: Guillaume Chauvon <94678394+Gchauvon@users.noreply.github.com>
    > Date:   Wed Jul 20 17:03:06 2022 +0200
    >
    >     decoder.sv: fix sfence.vma when rs1 != 0 (#933)
    >
    >     unlike other instructions with minor opcode == PRIV,
    >     SFENCE.VMA do not check for rs1 != 0.
    >     Only check for rd !=0 to raise illegal instruction
    >
    >     Signed-off-by: Guillaume Chauvon <guillaume.chauvon@thalesgroup.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cva6/__init__.py
+++ pythondata_cpu_cva6/__init__.py
@@ -4,42 +4,36 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cva6"
 
 # Module version
-version_str = "4.2.0.post411"
-version_tuple = (4, 2, 0, 411)
+version_str = "4.2.0.post412"
+version_tuple = (4, 2, 0, 412)
 try:
     from packaging.version import Version as V
-    pversion = V("4.2.0.post411")
+    pversion = V("4.2.0.post412")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "4.2.0.post269"
-data_version_tuple = (4, 2, 0, 269)
+data_version_str = "4.2.0.post270"
+data_version_tuple = (4, 2, 0, 270)
 try:
     from packaging.version import Version as V
-    pdata_version = V("4.2.0.post269")
+    pdata_version = V("4.2.0.post270")
 except ImportError:
     pass
-data_git_hash = "56ccf8089e80e5c7950038cad5796e50c64aa0c2"
-data_git_describe = "v4.2.0-269-g56ccf808"
+data_git_hash = "b6c1d04b6fa022c2e2df0b287865d28a0d42e756"
+data_git_describe = "v4.2.0-270-gb6c1d04b"
 data_git_msg = """\
-commit 56ccf8089e80e5c7950038cad5796e50c64aa0c2
-Author: Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
-Date:   Fri Jul 8 14:13:28 2022 +0200
+commit b6c1d04b6fa022c2e2df0b287865d28a0d42e756
+Author: Guillaume Chauvon <94678394+Gchauvon@users.noreply.github.com>
+Date:   Wed Jul 20 17:03:06 2022 +0200
 
-    Associated PRs in task.yaml (#929)
+    decoder.sv: fix sfence.vma when rs1 != 0 (#933)
     
-    * Removing CVA6-SDK from task.yaml
+    unlike other instructions with minor opcode == PRIV,
+    SFENCE.VMA do not check for rs1 != 0.
+    Only check for rd !=0 to raise illegal instruction
     
-    * Associated PRs in task.yaml
-    
-    This commit provide a new textarea to fill links to PRs used to complete the task.
-    
-    * Create bug.yaml
-    
-    * Update bug.yaml - Case consistency
-    
-    Co-authored-by: JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
+    Signed-off-by: Guillaume Chauvon <guillaume.chauvon@thalesgroup.com>
 
 """
 
