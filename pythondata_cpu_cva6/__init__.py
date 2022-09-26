44c6c6110fc04c7b49bf1eae1eb158e0aae411e5
LiteX Robot <robot@timvideos.us>
2022-09-26 13:50:46 +0000

    Updating pythondata-cpu-cva6 to 4.2.0.post426
    Updated data to v4.2.0-284-gb9cfd535 based on b9cfd53520c5ede0efbcbeab4379f85cd2d11d4c from https://github.com/openhwgroup/cva6.
    > commit b9cfd53520c5ede0efbcbeab4379f85cd2d11d4c
    > Author: JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
    > Date:   Mon Sep 26 14:52:52 2022 +0200
    >
    >     Fix Flist.cv32a60x to load the right config package (#964)
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cva6/__init__.py
+++ pythondata_cpu_cva6/__init__.py
@@ -4,34 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cva6"
 
 # Module version
-version_str = "4.2.0.post425"
-version_tuple = (4, 2, 0, 425)
+version_str = "4.2.0.post426"
+version_tuple = (4, 2, 0, 426)
 try:
     from packaging.version import Version as V
-    pversion = V("4.2.0.post425")
+    pversion = V("4.2.0.post426")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "4.2.0.post283"
-data_version_tuple = (4, 2, 0, 283)
+data_version_str = "4.2.0.post284"
+data_version_tuple = (4, 2, 0, 284)
 try:
     from packaging.version import Version as V
-    pdata_version = V("4.2.0.post283")
+    pdata_version = V("4.2.0.post284")
 except ImportError:
     pass
-data_git_hash = "6d263a6023298bf9d83aba44ddb1f30fd3ed90a9"
-data_git_describe = "v4.2.0-283-g6d263a60"
+data_git_hash = "b9cfd53520c5ede0efbcbeab4379f85cd2d11d4c"
+data_git_describe = "v4.2.0-284-gb9cfd535"
 data_git_msg = """\
-commit 6d263a6023298bf9d83aba44ddb1f30fd3ed90a9
-Author: Zbigniew Chamski <107464696+zchamski@users.noreply.github.com>
-Date:   Thu Sep 22 19:49:19 2022 +0200
+commit b9cfd53520c5ede0efbcbeab4379f85cd2d11d4c
+Author: JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
+Date:   Mon Sep 26 14:52:52 2022 +0200
 
-    compressed_decoder.sv: Fix FP word L/S decompression as per ISA spec v2.2. (#957)
-    
-    * compressed_decoder.sv: Fix of word L/S as per ISA spec v2.2.
-    * core/compressed_decoder.sv: Use word L/S patterns in FLW/FSW/FLWSP/FSWSP
-      expansions on RV32C.
+    Fix Flist.cv32a60x to load the right config package (#964)
 
 """
 
