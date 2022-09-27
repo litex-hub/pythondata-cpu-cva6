2b9a52845362760e8f6e0d844541c1d4c30840f4
LiteX Robot <robot@timvideos.us>
2022-09-27 13:51:50 +0000

    Updating pythondata-cpu-cva6 to 4.2.0.post427
    Updated data to v4.2.0-285-g871be7c7 based on 871be7c7943820ebe72c85c72f9af1164a50dc32 from https://github.com/openhwgroup/cva6.
    > commit 871be7c7943820ebe72c85c72f9af1164a50dc32
    > Author: Zbigniew Chamski <107464696+zchamski@users.noreply.github.com>
    > Date:   Tue Sep 27 14:53:06 2022 +0200
    >
    >     [tracing] VCS and Verilator support of waveform dumps. (#965)
    >
    >     Usage of the macros:
    >         * If defined, VM_TRACE enables tracing. If macro VM_TRACE_FST is not defined (the default), waveform generation will use VCD format. If the command-line option -v FILE or --vcd=FILE is given to the compiled simulator, the VCD output will be written to file named FILE in the current working dir of the verilated simulator. If no -v/--vcd= option is given on cmdline, or an FST-specific trace option is used, the simulator will generate a VCD trace according to the settings in the RTL code.
    >         * if VM_TRACE_FST is defined as well, then FST format is used instead of VCD. If the command line option -f FILE or --fst=FILE is given to the simulator, the trace will be stored in file FILE in the current working dir of the verilated simulator. If no -f/--fst= option is given, or a VCD-specific trace option is used, the simulator will generate an FST trace according to the settings in the RTL code.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cva6/__init__.py
+++ pythondata_cpu_cva6/__init__.py
@@ -4,30 +4,34 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cva6"
 
 # Module version
-version_str = "4.2.0.post426"
-version_tuple = (4, 2, 0, 426)
+version_str = "4.2.0.post427"
+version_tuple = (4, 2, 0, 427)
 try:
     from packaging.version import Version as V
-    pversion = V("4.2.0.post426")
+    pversion = V("4.2.0.post427")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "4.2.0.post284"
-data_version_tuple = (4, 2, 0, 284)
+data_version_str = "4.2.0.post285"
+data_version_tuple = (4, 2, 0, 285)
 try:
     from packaging.version import Version as V
-    pdata_version = V("4.2.0.post284")
+    pdata_version = V("4.2.0.post285")
 except ImportError:
     pass
-data_git_hash = "b9cfd53520c5ede0efbcbeab4379f85cd2d11d4c"
-data_git_describe = "v4.2.0-284-gb9cfd535"
+data_git_hash = "871be7c7943820ebe72c85c72f9af1164a50dc32"
+data_git_describe = "v4.2.0-285-g871be7c7"
 data_git_msg = """\
-commit b9cfd53520c5ede0efbcbeab4379f85cd2d11d4c
-Author: JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
-Date:   Mon Sep 26 14:52:52 2022 +0200
+commit 871be7c7943820ebe72c85c72f9af1164a50dc32
+Author: Zbigniew Chamski <107464696+zchamski@users.noreply.github.com>
+Date:   Tue Sep 27 14:53:06 2022 +0200
 
-    Fix Flist.cv32a60x to load the right config package (#964)
+    [tracing] VCS and Verilator support of waveform dumps. (#965)
+    
+    Usage of the macros:
+        * If defined, VM_TRACE enables tracing. If macro VM_TRACE_FST is not defined (the default), waveform generation will use VCD format. If the command-line option -v FILE or --vcd=FILE is given to the compiled simulator, the VCD output will be written to file named FILE in the current working dir of the verilated simulator. If no -v/--vcd= option is given on cmdline, or an FST-specific trace option is used, the simulator will generate a VCD trace according to the settings in the RTL code.
+        * if VM_TRACE_FST is defined as well, then FST format is used instead of VCD. If the command line option -f FILE or --fst=FILE is given to the simulator, the trace will be stored in file FILE in the current working dir of the verilated simulator. If no -f/--fst= option is given, or a VCD-specific trace option is used, the simulator will generate an FST trace according to the settings in the RTL code.
 
 """
 
