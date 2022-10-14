a15a3f618313887d3e24487e5a57f94aa45e85a5
LiteX Robot <robot@timvideos.us>
2022-10-14 06:04:42 +0000

    Updating pythondata-cpu-cva6 to 4.2.0.post431
    Updated data to v4.2.0-289-g4c01614f based on 4c01614f83cd9dd91d115aa190bd4fc6a284b59e from https://github.com/openhwgroup/cva6.
    > commit 4c01614f83cd9dd91d115aa190bd4fc6a284b59e
    > Author: Tianrui Wei <tianrui@tianruiwei.com>
    > Date:   Thu Oct 13 22:38:42 2022 -0700
    >
    >     Bump CVA6 for OpenPiton, fix mmu issues (#968)
    >
    >     This PR does the following
    >         1. Bump the filelist for OpenPiton for new directory layout
    >         2. Remove AXI Interface for OpenPiton in the top level
    >         3. Fix several issues in MMU discovered during address translation last year, the changes in core/mmu_sv39/mmu.sv are a joint effort between Jbalkind minho-pulp zarubaf niwis acostillado tianrui-wei
    >         4. disable bitmanip by default
    >         5. separate an ariane config package for openpiton synthesis. Some of  the previous changes makes ariane too big for openpiton, so we need to revert these changes
    >         6. Don't increase number of writeback ports (NR_WB_PORTS) when cvxif is  not enabled
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cva6/__init__.py
+++ pythondata_cpu_cva6/__init__.py
@@ -4,30 +4,38 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cva6"
 
 # Module version
-version_str = "4.2.0.post430"
-version_tuple = (4, 2, 0, 430)
+version_str = "4.2.0.post431"
+version_tuple = (4, 2, 0, 431)
 try:
     from packaging.version import Version as V
-    pversion = V("4.2.0.post430")
+    pversion = V("4.2.0.post431")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "4.2.0.post288"
-data_version_tuple = (4, 2, 0, 288)
+data_version_str = "4.2.0.post289"
+data_version_tuple = (4, 2, 0, 289)
 try:
     from packaging.version import Version as V
-    pdata_version = V("4.2.0.post288")
+    pdata_version = V("4.2.0.post289")
 except ImportError:
     pass
-data_git_hash = "df920cda7e4a8a0db7b6bcd0d2270a1b5a7521d2"
-data_git_describe = "v4.2.0-288-gdf920cda"
+data_git_hash = "4c01614f83cd9dd91d115aa190bd4fc6a284b59e"
+data_git_describe = "v4.2.0-289-g4c01614f"
 data_git_msg = """\
-commit df920cda7e4a8a0db7b6bcd0d2270a1b5a7521d2
-Author: JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
-Date:   Thu Oct 13 08:02:01 2022 +0200
+commit 4c01614f83cd9dd91d115aa190bd4fc6a284b59e
+Author: Tianrui Wei <tianrui@tianruiwei.com>
+Date:   Thu Oct 13 22:38:42 2022 -0700
 
-    Remove verilator_work files (#975)
+    Bump CVA6 for OpenPiton, fix mmu issues (#968)
+    
+    This PR does the following
+        1. Bump the filelist for OpenPiton for new directory layout
+        2. Remove AXI Interface for OpenPiton in the top level
+        3. Fix several issues in MMU discovered during address translation last year, the changes in core/mmu_sv39/mmu.sv are a joint effort between Jbalkind minho-pulp zarubaf niwis acostillado tianrui-wei
+        4. disable bitmanip by default
+        5. separate an ariane config package for openpiton synthesis. Some of  the previous changes makes ariane too big for openpiton, so we need to revert these changes
+        6. Don't increase number of writeback ports (NR_WB_PORTS) when cvxif is  not enabled
 
 """
 
