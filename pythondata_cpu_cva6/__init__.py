bda72a9c662d32a7c2b87a0b211a8ea5b6ab5c9a
LiteX Robot <robot@timvideos.us>
2022-10-22 06:22:47 +0000

    Updating pythondata-cpu-cva6 to 4.2.0.post432
    Updated data to v4.2.0-290-gf346d660 based on f346d6604f158a60a77e032f7396d5ac6a78e4aa from https://github.com/openhwgroup/cva6.
    > commit f346d6604f158a60a77e032f7396d5ac6a78e4aa
    > Author: Jérôme Quévremont <jerome.quevremont@thalesgroup.com>
    > Date:   Sat Oct 22 08:22:21 2022 +0200
    >
    >     Scratchpad can also be separate from caches (#981)
    >
    >     To ease the implementation. As we'll target FPGA and ASIC (not ASSP), we do not need a "one size fits all" solution.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cva6/__init__.py
+++ pythondata_cpu_cva6/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cva6"
 
 # Module version
-version_str = "4.2.0.post431"
-version_tuple = (4, 2, 0, 431)
+version_str = "4.2.0.post432"
+version_tuple = (4, 2, 0, 432)
 try:
     from packaging.version import Version as V
-    pversion = V("4.2.0.post431")
+    pversion = V("4.2.0.post432")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "4.2.0.post289"
-data_version_tuple = (4, 2, 0, 289)
+data_version_str = "4.2.0.post290"
+data_version_tuple = (4, 2, 0, 290)
 try:
     from packaging.version import Version as V
-    pdata_version = V("4.2.0.post289")
+    pdata_version = V("4.2.0.post290")
 except ImportError:
     pass
-data_git_hash = "4c01614f83cd9dd91d115aa190bd4fc6a284b59e"
-data_git_describe = "v4.2.0-289-g4c01614f"
+data_git_hash = "f346d6604f158a60a77e032f7396d5ac6a78e4aa"
+data_git_describe = "v4.2.0-290-gf346d660"
 data_git_msg = """\
-commit 4c01614f83cd9dd91d115aa190bd4fc6a284b59e
-Author: Tianrui Wei <tianrui@tianruiwei.com>
-Date:   Thu Oct 13 22:38:42 2022 -0700
+commit f346d6604f158a60a77e032f7396d5ac6a78e4aa
+Author: Jérôme Quévremont <jerome.quevremont@thalesgroup.com>
+Date:   Sat Oct 22 08:22:21 2022 +0200
 
-    Bump CVA6 for OpenPiton, fix mmu issues (#968)
+    Scratchpad can also be separate from caches (#981)
     
-    This PR does the following
-        1. Bump the filelist for OpenPiton for new directory layout
-        2. Remove AXI Interface for OpenPiton in the top level
-        3. Fix several issues in MMU discovered during address translation last year, the changes in core/mmu_sv39/mmu.sv are a joint effort between Jbalkind minho-pulp zarubaf niwis acostillado tianrui-wei
-        4. disable bitmanip by default
-        5. separate an ariane config package for openpiton synthesis. Some of  the previous changes makes ariane too big for openpiton, so we need to revert these changes
-        6. Don't increase number of writeback ports (NR_WB_PORTS) when cvxif is  not enabled
+    To ease the implementation. As we'll target FPGA and ASIC (not ASSP), we do not need a "one size fits all" solution.
 
 """
 
